8bd6937ef7e7562e1106e4213f97c6f89d2e61e8
nisshi_80a <nishimuraworks@nishimuranoMacBook-Air.local>
2026-08-11 14:56:00 +0900

    ログイン機能のsession処理とlogin画面の実装

--- config/routes.rb
+++ config/routes.rb
@@ -14,5 +14,9 @@ Rails.application.routes.draw do
   # root "posts#index"
   resources :users
 
+  get    'login',  to: 'sessions#new'
+  post   'login',  to: 'sessions#create'
+  delete 'logout', to: 'sessions#destroy'
+
 
 end
