d05836f30604d23608c01ab6dbbe6a67175f69dc
nisshi_80a <nishimuraworks@nishimuranoMacBook-Air.local>
2026-08-13 22:16:28 +0900

    レビューの投稿処理を実装

--- app/controllers/reviews_controller.rb
+++ app/controllers/reviews_controller.rb
@@ -6,8 +6,22 @@ before_action :require_login, only: [:new, :create]
   end
 
   def create
+   @review = current_user.reviews.build(review_params)
+    if @review.save
+        redirect_to root_path, notice: "レビューを投稿しました"
+    else
+        flash.now[:alert] = "レビューの投稿に失敗しました。入力内容を確認してください。"
+        render :new, status: :unprocessable_entity
+    end
+
   end
+ 
+  private
   
+  def review_params
+    params.require(:review).permit(:title, :genre, :author, :comment, :impression)
+  end
+
 
 
 end
